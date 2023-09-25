8b3727312db5d8b44b02092f3828ba39218beb47
Gabrielsandres <gabrielsandresrocha@gmail.com>
2023-09-25 18:40:58 -0300

    TPO 03

---
+++ TPO03/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dados.h"  
+#include "calculo.c"  
+
+int main() {
+
+    FILE *file;
+    int numVertices;
+    Ponto *vertices;
+    float area = 0.0;
+
+    file = fopen("vertices.txt", "r");
+    
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo de entrada.\n");
+        return 1;
+    }
+
+    fscanf(file, "%d", &numVertices);
+
+    if (numVertices < 3) {
+        printf("O poligono deve ter pelo menos 3 vertices.\n");
+        fclose(file);
+        return 1;
+    }
+
+    vertices = (Ponto *)malloc(numVertices * sizeof(Ponto));
+    if (vertices == NULL) {
+        printf("Erro ao alocar memoria para os vertices.\n");
+        fclose(file);
+        return 1;
+    }
+
+    for (int i = 0; i < numVertices; i++) {
+        fscanf(file, "%f %f", &(vertices[i].X), &(vertices[i].Y));
+    }
+    
+    fclose(file);
+
+    area = calcularArea(vertices, numVertices);
+
+    free(vertices);
+
+    printf("A area do poligono eh: %.2f\n", area);
+
+    return 0;
+}
